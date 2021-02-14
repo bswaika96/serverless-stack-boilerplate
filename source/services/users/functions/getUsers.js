23f236b4e57bed3133d4af44be683f50bbc5d3d3
Baladitya Swaika <bswaika96@gmail.com>
2021-02-14 14:58:56 +0530

    Added a basic lambda for testing. Throws an Error on a custom header if present to test Error Propagation back to the Handler

---
+++ source/services/users/functions/getUsers.js
@@ -0,0 +1,22 @@
+import AppError from '../../../shared/libs/errors/AppErrors';
+import handler from '../../../shared/libs/GlobalHandler';
+
+const users = [{
+    name: 'Baladitya',
+    age: 25
+},{
+    name: 'Shweta',
+    age: 29
+}];
+
+const lambda = async(request) => {
+    if(request.headers.Throw == 'Error'){
+        throw new AppError(500, 'Internal Server Error');
+    }
+    return {
+        status: 200,
+        data: users
+    }
+}
+
+export const main = handler(lambda)
