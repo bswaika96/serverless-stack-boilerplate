f7e3942fa01b645f6eff5546ab3b841dec24fe69
Baladitya Swaika <bswaika96@gmail.com>
2021-02-14 13:55:25 +0530

    Fixed index.js for hosting

--- docs/index.js
+++ docs/index.js
@@ -3,12 +3,13 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
 const FILENAME = './swagger.yml';
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const doc = YAML.load(FILENAME);
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(doc));
 
-app.listen(3000, () => {
-    console.log(`docs server running at PORT 3000...`);
+app.listen(PORT, () => {
+    console.log(`docs server running at PORT ${PORT}...`);
 });
